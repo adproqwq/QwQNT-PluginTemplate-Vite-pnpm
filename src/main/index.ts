4a59158e25da12141c7230bae1b46a71d1dba2de
Adpro <101449500+adproqwq@users.noreply.github.com>
2025-07-24 21:43:41 +0800

    perf: 使用 peek 不再返回 window

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,9 +4,7 @@ ipcMain.on('QwQNTTemplate.greeting', () => {
   alert('Main');
 });
 
-qwqnt.main.hooks.whenBrowserWindowCreated.on(window => {
+qwqnt.main.hooks.whenBrowserWindowCreated.peek(window => {
   console.log('A window has just been created');
   console.log(window);
-
-  return window;
 });
