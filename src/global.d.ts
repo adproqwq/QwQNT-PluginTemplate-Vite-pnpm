a55594334508c2102b9d0e898320c3361942e898
Adpro <101449500+adproqwq@users.noreply.github.com>
2025-08-01 00:44:11 +0800

    feat: new sample

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
+declare type PluginSelf = import('./types-context-DXNxnjxt.js').P
 
 declare namespace QwQNTTemplate {
   const greeting: () => void;
--- src/main/index.ts
+++ src/main/index.ts
@@ -8,3 +8,7 @@ qwqnt.main.hooks.whenBrowserWindowCreated.peek(window => {
   console.log('A window has just been created');
   console.log(window);
 });
+
+export const onLoad = (self: PluginSelf) => {
+  console.log('Loaded', self);
+};
