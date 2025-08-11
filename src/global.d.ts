9a61adef8e9d17fe33450ab6e3cf4cee06139eb8
風間青祢 <101449500+adproqwq@users.noreply.github.com>
2025-08-11 09:33:41 +0800

    feat: adapt new build

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-declare type PluginSelf = import('./types-context-BASJpFP5.js').P
+declare type PluginSelf = import('./types/qwqnt/types-context-D3vGm2Vy').P
 
 declare namespace QwQNTTemplate {
   const greeting: () => void;
--- src/preload/preload.d.ts
+++ src/preload/preload.d.ts
@@ -1,16 +1,1 @@
-import { C as ContextGlobal, P as Plugin } from '../types-context-D3vGm2Vy.js';
-
-declare function ipcRendererImport(channel: string): any;
-
-interface ContextPreload extends ContextGlobal {
-    preload: {
-        import: typeof ipcRendererImport;
-    };
-}
-declare global {
-    const qwqnt: Readonly<ContextPreload>;
-    const __self: Plugin;
-    function evalModule(script: string, self?: unknown): void;
-}
-
-export type { ContextPreload };
+import '../types/qwqnt/preload';
